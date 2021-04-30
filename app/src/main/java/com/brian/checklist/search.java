5a76a5d47eb4bc6e70352df3167081c052c265f7
cathe <catherinesurvive@163.com>
2021-04-30 13:08:12 +0800

    初步完成搜索功能

--- app/src/main/java/com/brian/checklist/search.java
+++ app/src/main/java/com/brian/checklist/search.java
@@ -18,7 +18,7 @@ public class search extends AppCompatActivity {
     private MyDatabaseDAO db;
     private ListView listView;
     private ListViewAdapter adapter;
-    private List<Map<String ,Object>> datalist;
+    private List<Map<String, Object>> datalist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +30,17 @@ public class search extends AppCompatActivity {
         searchList.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_SEARCH || (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
                     String listName = searchList.getText().toString().trim();
-                    if(listName.length()!=0){
+                    if (listName.length() != 0) {
 
-                        datalist=getData(listName);
-                        if (datalist!=null){
-                            adapter = new ListViewAdapter(search.this,datalist);
+                        datalist = getData(listName);
+                        if (datalist != null) {
+                            adapter = new ListViewAdapter(search.this, datalist);
                             listView.setAdapter(adapter);
+                        } else {
+                            Log.v("111", "null");
                         }
-                        else {
-                            Log.v("111","null");
-                        }
-
                     }
                 }
                 return false;
@@ -50,7 +48,7 @@ public class search extends AppCompatActivity {
         });
     }
 
-    public List<Map<String ,Object>> getData(String name){
+    public List<Map<String, Object>> getData(String name) {
         return db.searchList(name);
     }
 
